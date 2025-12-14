a4fc60e8bbb6c5364734100e30bb4c2f279da1b0
Khadejah <136977286+kkhaddejah@users.noreply.github.com>
2025-12-14 08:53:43 +0400

    memcp

--- libft/ft_memcp.c
+++ libft/ft_memcp.c
@@ -9,6 +9,18 @@ void ft_memcpy(void *dest, void *src, int n)
 
 	for (int i = 0; i < n; i++)
 		char_dest[i] = char_src[i];
+}
 
-	return dest;
+int main() {
+	char src[] = "Hello, World!";
+	char dest[20];
+
+	ft_memcpy(dest, src, 13);
+	dest[13] = '\0'; // Null-terminate the destination string
+
+	write(1, dest, 13); // Output: Hello, World!
+	write(1, "\n", 1);
+	write(1, src, 13);  // Output: Hello, World!
+
+	return 0;
 }
